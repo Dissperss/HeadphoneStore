0f61cabe9615d7fed30eea21c71787da415e68f9
Dissperss <subbotin.vladimir2018@ya.ru>
2026-01-14 18:13:17 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from "react";
+import { Footer } from "./components/layout";
+
+function App() {
+    return (
+        <>
+            <div>!</div>
+        </>
+    );
+}
+
+export default App;
+
+// Везде именнованый экспорт
