4638f218ad60d1e462fa52e5faeb196c81bb83f8
Dissperss <subbotin.vladimir2018@ya.ru>
2026-01-19 14:57:05 +0300

    fix: Styles for ProductCard & ProductList

--- src/components/productCard/ProductCard.tsx
+++ src/components/productCard/ProductCard.tsx
@@ -1,6 +1,6 @@
-import styles from "./ProductCard.module.css";
-
 import { StarIcon } from "../icons";
+
+import styles from "./ProductCard.module.css";
 
 type ProductCardProps = {
     img: string;
@@ -18,8 +18,14 @@ export const ProductCard = ({ img, title, price, rate }: ProductCardProps) => {
     const { actual, previous } = price;
     return (
         <div className={styles.card}>
-            <div className={styles["card__inner"]}>
-                <img className={styles["card__img"]} src={img} alt={title} />
+            <div className={styles.card__inner}>
+                <div className={styles["card__img-wrapper"]}>
+                    <img
+                        className={styles["card__img"]}
+                        src={img}
+                        alt={title}
+                    />
+                </div>
                 <div className={styles["card__info"]}>
                     <h3 className={styles["card__title"]}>{title}</h3>
                     <div className={styles["card__prices"]}>
@@ -33,14 +39,14 @@ export const ProductCard = ({ img, title, price, rate }: ProductCardProps) => {
                         )}
                     </div>
                 </div>
+            </div>
 
-                <div className={styles["card__footer"]}>
-                    <div className={styles.rate}>
-                        <StarIcon />
-                        <span className={styles["card__rating"]}>{rate}</span>
-                    </div>
-                    <button className={styles["card__btn"]}>Купить</button>
+            <div className={styles["card__footer"]}>
+                <div className={styles.rate}>
+                    <StarIcon />
+                    <span className={styles["card__rating"]}>{rate}</span>
                 </div>
+                <button className={styles["card__btn"]}>Купить</button>
             </div>
         </div>
     );
--- src/components/productList/ProductList.tsx
+++ src/components/productList/ProductList.tsx
@@ -1,4 +1,5 @@
 import { ProductCard } from "../productCard";
+import styles from "./ProductList.module.css";
 
 type ProductListProps = {
     data: Product[];
@@ -20,10 +21,18 @@ export const ProductList = ({ data }: ProductListProps) => {
         return <p className="nothing">Nothing found</p>;
     }
 
-    const elements = data.map((item) => {
-        const { id, ...itemProps } = item;
-        return <ProductCard key={id} {...itemProps} />;
-    });
+    // const elements = data.map((item) => {
+    //     const { id, ...itemProps } = item;
+    //     return <ProductCard key={id} {...itemProps} />;
+    // });
 
-    return <ul className="products__list">{elements}</ul>;
+    return (
+        <ul className={styles["products__list"]}>
+            {data.map(({ id, ...itemProps }) => (
+                <li key={id} className={styles["products__item"]}>
+                    <ProductCard {...itemProps} />
+                </li>
+            ))}
+        </ul>
+    );
 };
